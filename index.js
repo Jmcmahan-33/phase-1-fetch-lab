146b4a945f2544f20ecb7cc24c9d34fed76fc8bf
Jared McMahan <jaredm788@gmail.com>
2022-09-06 10:42:38 -0600

    commit tree 022c57816db54504cc5b3f40b789a2b66fbb8e0d

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+// function fetchBooks() {
+//     fetch("https://anapioficeandfire.com/api/books")
+//   .then(function (res) {
+//     return res.json()
+//   })
+//   .then(function (json) {
+//     console.log(json)
+//      return renderBooks(json)
+//   })
+// }
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
